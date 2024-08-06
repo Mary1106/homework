6dcb3aadcb5e29dbbdc8f1f7835250015ab6c225
Maria Maltseva <mariamaltseva1106@gmail.com>
2024-08-06 14:54:00 +0300

    added def in widget

---
+++ src/widget.py
@@ -0,0 +1,19 @@
+from src.masks import get_mask_card_number
+from src.masks import get_mask_account
+
+def mask_account_card(account_card: str) -> str | None:
+    """Принимает на вход тип карты и ее номер или счет и его номер; возвращает маску карты или счета"""
+    account_card_splited = account_card.split()
+    if len(account_card_splited[-1]) == 16:
+        return f"{account_card_splited[0]} {account_card_splited[1]} {get_mask_card_number(account_card_splited[2])}"
+    elif len(account_card_splited[-1]) == 20:
+        return f"{account_card_splited[0]} {get_mask_account(account_card_splited[1])}"
+    else:
+        return None
+
+def get_date(date_time: str) -> str | None:
+    date_time_splited = date_time.split("T")
+    return
+
+date_time = "2024-03-11T02:26:18.671407"
+print(get_date(date_time))
