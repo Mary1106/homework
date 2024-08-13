5255cd4da0eaf302f17e757adc3fc798ce34fad7
Maria Maltseva <mariamaltseva1106@gmail.com>
2024-08-13 20:34:11 +0300

    fixed function in widget.py

--- src/widget.py
+++ src/widget.py
@@ -6,7 +6,10 @@ def mask_account_card(account_card: str) -> str | None:
     """Принимает на вход тип карты и ее номер или счет и его номер; возвращает маску карты или счета"""
     account_card_splited = account_card.split()
     if len(account_card_splited[-1]) == 16:
-        return f"{account_card_splited[0]} {account_card_splited[1]} {get_mask_card_number(account_card_splited[2])}"
+        if len(account_card.split()) == 3:
+            return f"{account_card_splited[0]} {account_card_splited[1]} {get_mask_card_number(account_card_splited[-1])}"
+        else:
+            return f"{account_card_splited[0]} {get_mask_card_number(account_card_splited[1])}"
     elif len(account_card_splited[-1]) == 20:
         return f"{account_card_splited[0]} {get_mask_account(account_card_splited[1])}"
     else:
