dc82a60ad09ae55d177518a7e5460721762c8631
Maria Maltseva <mariamaltseva1106@gmail.com>
2024-08-23 12:17:55 +0300

    decorator tested

---
+++ tests/test_decorators.py
@@ -0,0 +1,18 @@
+import pytest
+from src.decorators import log, my_function
+
+def test_log_error(capsys):
+    with pytest.raises(Exception):
+        captured = capsys.readouterr()
+        assert captured.out == f"my_function is not ok: {error}. Inputs:{args}, {kwargs}\n"
+    @log
+    def my_function(x = "1", y = 1):
+        return x + y
+
+def test_log_ok(capsys):
+    with pytest.raises(Exception):
+        captured = capsys.readouterr()
+        assert captured.out == f"my_function is ok\n"
+    @log
+    def my_function(x = 1, y = 1):
+        return x + y
